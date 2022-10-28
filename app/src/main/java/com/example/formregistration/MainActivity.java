84e87c2da6fcc74e7ba86cf96a6098bdf0dde250
Muhammad Adil Raja Saputra <adilraja19@students.undip.ac.id>
2022-10-28 09:14:40 +0700

    Perbaikan minor pada margin dan hapus import ga terpakai

--- app/src/main/java/com/example/formregistration/MainActivity.java
+++ app/src/main/java/com/example/formregistration/MainActivity.java
@@ -6,8 +6,6 @@ import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
